07da27c1d0efef6268b98dd54f42dde7bd208e10
abhishek0400 <abhishekupadhyay040@gmail.com>
2025-09-27 11:26:22 +0530

    add output for s3

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,25 @@
-output "cluster_name" {
-  description = "Amazon Web Service EKS Cluster Name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "Amazon Web Service EKS Cluster Name"
+#   value       = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  description = "Endpoint for Amazon Web Service EKS "
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "Endpoint for Amazon Web Service EKS "
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "region" {
-  description = "Amazon Web Service EKS Cluster region"
-  value       = var.region
-}
+# output "region" {
+#   description = "Amazon Web Service EKS Cluster region"
+#   value       = var.region
+# }
 
 
-output "cluster_security_group_id" {
-  description = "Security group ID for the Amazon Web Service EKS Cluster "
-  value       = module.eks.cluster_security_group_id
+# output "cluster_security_group_id" {
+#   description = "Security group ID for the Amazon Web Service EKS Cluster "
+#   value       = module.eks.cluster_security_group_id
+# }
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.vprofile_bucket.bucket
 }
